1a0f041ee06ab70ce5c0c9605f375a3e59466726
Brian Goff <cpuguy83@gmail.com>
2011-09-15 18:03:55 -0400

    Add routes for work orders, changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Otrs::Application.routes.draw do
     resources :tickets do
       resource :articles
     end
+    resources :changes do
+      resource :work_orders
+    end
     resources :config_items
   end
   resources :tickets
   resources :links
   resources :config_items
   resources :articles
+  resources :changes
+  resources :work_orders
 end
