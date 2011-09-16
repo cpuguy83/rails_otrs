272a6b8e1fb64ffcb867aed21db244ce34702434
Brian Goff <cpuguy83@gmail.com>
2011-09-16 11:53:58 -0400

    Initial commit
    Added this specifically for querying the available ticket types,
    currently just all ticket types.

---
+++ app/models/otrs/ticket/type.rb
@@ -0,0 +1,40 @@
+class OTRS::Ticket::Type < OTRS::Ticket
+  
+  def self.set_accessor(key)
+    attr_accessor key.to_sym
+  end
+  
+  def persisted?
+    false
+  end
+  
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      OTRS::Ticket.set_accessor(name.to_s.underscore)
+      send("#{name.to_s.underscore.to_sym}=", value)
+    end
+  end
+  
+  def self.all
+    params = 'Object=TicketObject&Method=TicketTypeList&Data={"UserID":"1"}'
+    a = connect(params)
+    a = Hash[*a]
+    b = []
+    a.each do |key,value|
+      c = {}
+      c[key] = value
+      b << c
+    end
+    c = []
+    b.each do |d|
+      d.each do |key,value|
+        tmp = {}
+        tmp[:id] = key
+        tmp[:name] = value
+        c << new(tmp)
+      end
+    end
+    c
+  end
+  
+end
